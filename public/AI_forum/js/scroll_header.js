eb23a06773fe42230f1b9dfef2ba9326eb85d0d1
digi-the-design <rikiyasasaki@hotmail.com>
2025-11-17 23:29:16 +0900

    初回

--- public/AI_forum/js/scroll_header.js
+++
@@ -1,9 +0,0 @@
-  $(window).scroll(function() {
-	if ($(this).scrollTop() < 730) {
-	  $('.scroll_header').css('top', '-100px');
-	  $('.fixed_header').css('visibility', 'visible');
-	} else {
-		$('.scroll_header').css('top', '0px');
-		$('.fixed_header').css('visibility', 'hidden');
-	}
-  });
