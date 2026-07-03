bddb30482f59ef0c89567440ff2571efbe6acda0
rikiyasasaki <rikiyasasaki@hotmail.com>
2026-07-03 18:38:56 +0900

    7章

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -11,7 +11,7 @@ type Props = {
   // Promise；<{ slug: string }> TypeScriptの「ジェネリクス」機能
   params: Promise<{ slug: string }>;
   // microCMSのプレビュー機能対応。クエリパラメータを受け取るための型定義
-  searchParams: Promise<{ draftKey?: string }>;
+  searchParams: Promise<{ dk?: string }>;
 };
 
 // ⬛︎ ページを動的：SSR（Server Side Rendering）に変更してmicroCMS即時反映（キャッシュなし）
@@ -25,10 +25,10 @@ export default async function Page({ params, searchParams }: Props) {
   // データを受取るまでawaitで解決しslug属性に分割代入
   const { slug } = await params;
   // データを受取るまでawaitで解決しdk属性に分割代入
-  const { draftKey } = await searchParams;
+  const { dk } = await searchParams;
   // microCMS側URL設定:{CONTENT_ID}?dk={DRAFT_KEY} をコード化:(slug,{draftkey:dk})
   const data = await getNewsDetail(slug, {
-    draftKey,
+    draftKey: dk,
   }).catch(notFound);
   //記事：Articleコンポーネント・一覧に戻るボタン：ButtonLinkコンポーネント追加
   return (
