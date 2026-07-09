1ecd8a7421536527a67ae5190b0a9eb05364aa9f
rikiyasasaki <rikiyasasaki@hotmail.com>
2026-07-09 19:19:38 +0900

    8章

--- middleware.ts
+++ middleware.ts
@@ -3,4 +3,4 @@ import { createNextAuthMiddleware } from "nextjs-basic-auth-middleware";
 export const middleware = createNextAuthMiddleware();
 
 // matcherで指定したパス(任意のページ、複数指定可能)に対してのみこのプロクシが適用されます
-export const config = { matcher: ["/news/:path*"] };
+export const config = { matcher: ["/news", "/news/:path*"] };
