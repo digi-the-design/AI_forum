86514f27cba8e06246578148131338aaba05e71c
rikiyasasaki <rikiyasasaki@hotmail.com>
2026-09-15 12:23:21 +0900

    AI_forum

--- app/(pages)/news/layout.tsx
+++ app/(pages)/news/layout.tsx
@@ -3,14 +3,17 @@ import "@/app/globals.css";
 import Hero from "@/app/_components/Hero_A";
 import styles from "./page.module.css";
 
+type NewsLayoutProps = Readonly<{
+  children: React.ReactNode;
+}>;
 
 export const metadata: Metadata = {
   title: "NEWS | 新着ニュース記事一覧",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
+export const revalidate = 0;
+
+export default function RootLayout({ children }: NewsLayoutProps) {
   return (
     <>
       <Hero
